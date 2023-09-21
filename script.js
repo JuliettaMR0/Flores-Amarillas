006a716c09fee9695b3a3117d7880d827f1299c4
JuliettaMR0 <mjulietarouzies2402@gmail.com>
2023-09-21 19:37:46 -0300

    actuN2

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-<<<<<<< HEAD
+// Selección de elementos
 const flowers = document.querySelectorAll(".flower");
 const flowerTexts = [
-  "No importa cuantas flores ponga.",
-  "Ninguna va a llegar hacer.",
+  "No importa cuántas flores ponga.",
+  "Ninguna va a llegar a ser",
   "Tan bonita como vos.",
 ];
 
+// Función para mostrar el texto de la flor
+function toggleFlowerText(index) {
+  flowers.forEach((flower, otherIndex) => {
+    const flowerText = flower.querySelector(".flower-text");
+    if (otherIndex !== index) {
+      flower.classList.remove("blooming");
+      flowerText.style.opacity = 0;
+    }
+  });
+
+  const flower = flowers[index];
+  const flowerText = flower.querySelector(".flower-text");
+  flower.classList.toggle("blooming");
+
+  if (flower.classList.contains("blooming")) {
+    flowerText.style.opacity = 1;
+  } else {
+    flowerText.style.opacity = 0;
+  }
+}
+
+// Agregar evento de clic a cada flor
 flowers.forEach((flower, index) => {
   const flowerText = document.createElement("div");
   flowerText.classList.add("flower-text");
@@ -13,67 +35,11 @@ flowers.forEach((flower, index) => {
   flower.appendChild(flowerText);
 
   flower.addEventListener("click", () => {
-    flowers.forEach((otherFlower, otherIndex) => {
-      if (otherIndex !== index) {
-        otherFlower.classList.remove("blooming");
-        otherFlower.querySelector(".flower-text").style.opacity = 0;
-      }
-    });
-
-    flower.classList.toggle("blooming");
-
-    if (flower.classList.contains("blooming")) {
-      flowerText.style.opacity = 1;
-    } else {
-      flowerText.style.opacity = 0;
-    }
+    toggleFlowerText(index);
   });
-=======
-// const flowers = document.querySelectorAll('.flower');
-
-// flowers.forEach(flower => {
-//     flower.addEventListener('click', () => {
-//         flower.classList.add('blooming');
-//         setTimeout(() => {
-//             flower.classList.remove('blooming');
-//         }, 1000); // La animación dura 1 segundo (1000 milisegundos)
-//     });
-// });
-const flowers = document.querySelectorAll('.flower');
-const flowerTexts = [
-    'No importa cuantas flores ponga.',
-    'Ninguna va a llegar hacer.',
-    'Tan bonita como vos.'
-];
-
-flowers.forEach((flower, index) => {
-    const flowerText = document.createElement('div');
-    flowerText.classList.add('flower-text');
-    flowerText.textContent = flowerTexts[index];
-    flower.appendChild(flowerText);
-    
-    flower.addEventListener('click', () => {
-        flowers.forEach((otherFlower, otherIndex) => {
-            if (otherIndex !== index) {
-                otherFlower.classList.remove('blooming');
-                otherFlower.querySelector('.flower-text').style.opacity = 0;
-            }
-        });
-        
-        flower.classList.toggle('blooming');
-        
-        if (flower.classList.contains('blooming')) {
-            flowerText.style.opacity = 1;
-        } else {
-            flowerText.style.opacity = 0;
-        }
-    });
->>>>>>> 6eb106dec2258e8774022038407033be9a049b6c
 });
 
-/* PARTE DE LA GALERIA, AIUDA NOS E QUEH AGO */
-
-<<<<<<< HEAD
+// Galería de imágenes
 const imageGallery = document.querySelector(".image-gallery");
 const images = imageGallery.querySelectorAll("img");
 let currentImageIndex = 0;
@@ -85,26 +51,4 @@ function showNextImage() {
 }
 
 // Mostrar la primera imagen al cargar la página
-images[currentImageIndex].classList.add("active");
-=======
-const imageGallery = document.querySelector('.image-gallery');
-const images = imageGallery.querySelectorAll('img');
-let currentImageIndex = 0;
-
-function showNextImage() {
-    images[currentImageIndex].classList.remove('active');
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    images[currentImageIndex].classList.add('active');
-}
-
-// Mostrar la primera imagen al cargar la página
-images[currentImageIndex].classList.add('active');
->>>>>>> 6eb106dec2258e8774022038407033be9a049b6c
-
-// Cambiar la imagen cada 3 segundos (3000 milisegundos)
-setInterval(showNextImage, 3000);
-
-<<<<<<< HEAD
-/*FIN DE LA ANIMACION DE LAS FLORES*/
-=======
->>>>>>> 6eb106dec2258e8774022038407033be9a049b6c
+images[currentImageIndex].classList;
